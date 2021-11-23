336abe0996b8425a54205eb19ba5907a4782179d
Jaskaran Sohi <jaskaran.sohi@gmail.com>
2021-11-22 17:14:00 -0800

    commit tree 80706d205cdf056f28bb1f482d78c382be2536e2

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+      return 'This one is on me!';
+  } else if (distance > 2000 && distance < 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else if (distance >= 2500) {
+      return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+// const order = 'cheeseburger';
+
+// let ingredients;
+
+// switch (order) {
+//     case 'cheeseburger':
+//         ingredients = 'bun, burger, cheese, lettuce, tomato, onion';
+//         break;
+//     case 'hamburger':
+//         ingredients = 'bun, burger, lettuce, tomato, onion';
+//         break;
+//     case 'malted':
+//         ingredients = 'milk, ice cream, malted milk powder';
+//         break;
+//     default:
+//         console.log("Sorry, that's not on the menu right now.");
+//         break;
+// }
